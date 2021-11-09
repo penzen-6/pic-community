c3750409a8556881ed3cc9e9d6fdccdd7f42ebab
kaito haruguchi <penzen_6@icloud.com>
2021-11-09 14:32:47 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  resources :posts do
-    resources :comments, only: [:create]
-  end
-
-  resources :likes, only: [:create, :destroy]
+  resources :posts
   
   resources :profiles, only: [:show, :new, :edit, :create, :update]
   root 'welcome#index'
